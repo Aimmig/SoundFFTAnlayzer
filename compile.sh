226264a88ab301bab3499fe5920e70ba3e8f2337
Andre Immig <aimmig@students.uni-mainz.de>
2018-09-30 20:38:17 +0200

    Basic commit with working fft for wav

---
+++ compile.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+sfml ="-lsfml-graphics -lsfml-window -lsfml-system -lsfml-audio"
+g++ -Wall -Wextra -c main.cpp $sfml
+g++ -Wall -Wextra -c FFT.cpp $sfml
+g++ -Wall -Wextra -g -o main FFT.o main.o -lsfml-graphics -lsfml-window -lsfml-system -lsfml-audio
